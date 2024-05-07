72d43c5c762ccbbc6fc4507abb43cd0a0cf099b7
aasimdev <aasimdev69@gmail.com>
2024-05-07 23:54:01 +0500

    added newnews

---
+++ digital-locations-joins-with-industry-heavyweights-as-a-member-of-the-newly-formed-mobile-satellite-services-association.php
@@ -0,0 +1,53 @@
+<?php
+$GLOBALS['title'] = "Digital Locations Joins with Industry Heavyweights as a Member of the Newly Formed Mobile Satellite Services Association - Digital Locations";
+$GLOBALS['desc'] = "";
+$GLOBALS['keywords'] = "";
+include('header.php'); ?>
+
+<section class="mbanner nographic" style="background-image: url(./assets/img/market-bg.png);">
+    <div class="container">
+        <h1>Newsroom</h1>
+    </div>
+</section>
+
+<div class="cde__content">
+    <div class="container">
+        <h3>Digital Locations Joins with Industry Heavyweights as a Member of the Newly Formed Mobile Satellite Services Association </h3>
+        <span>May 7, 2024</span>
+
+        <p>SANTA BARBARA, CA, May 7, 2024 (GLOBE NEWSWIRE) – Digital Locations, Inc. (OTCMKTS:DLOC), the developer of SatennaTM, a breakthrough technology that will deliver high-speed Internet from satellites directly to smartphones all over the world, today announced that it has joined the Mobile Satellite Services Association (MSSA), the newest satellite industry association.  </p>
+
+        <p>After Viasat Terrestar Solutions, Ligado Networks, Omnispace, and Al Yah Satellite Communications jointly announced the establishment of the MSSA, Digital Locations immediately joined the organization. The MSSA aims to significantly enhance scale and choice to foster and propel the emerging Direct-to-Device (D2D) ecosystem.</p>
+
+        <p>The MSSA aims to cultivate a global ecosystem leveraging over 100 MHz of L- and S-band spectrum already designated and licensed for mobile satellite services (MSS), ideally suited for integration into a wide array of mobile devices. This non-profit industry association intends to adhere to 3rd Generation Partnership Project (3GPP) standards to extend terrestrial mobile coverage for both Mobile Network Operator (MNO) and Over-the-Top (OTT) internet services. </p>
+
+
+        <p>Among the main objectives are interoperable architectures and standards for utilisation in multi-orbit satellite systems, ground infrastructure, and end-user equipment, and augmenting seamless global roaming between terrestrial and multi-orbit satellite networks through the development of recommended specifications.</p>
+
+<p>“Integrating satellite connectivity into consumer mobile devices is a transformative opportunity for the satellite industry. As a coalition of leaders with a unified voice, MSSA will be a driving force in making this new marketplace a reality, while respecting the rights of nations to meaningfully engage and retain sovereignty in a rapidly growing space economy,” said Mark Dankberg, Chairman of MSSA, and Chairman and CEO of Viasat. “The Association will help create opportunities to scale NTN systems via open, standards-based solutions for D2D and IoT. We believe this can be done while preserving critical MSS aviation and maritime safety services and by expanding the markets supporting diverse users through substantial improvements in speed, bandwidth, availability, interoperability and affordability.”</p>
+
+        <p>Rich Berliner, CEO of Digital Locations, commented “We are happy to join these industry heavyweights, as the goals and aspirations of the MSSA are closely aligned with our development of Satenna™. We see the membership and the priorities of the MSSA as well balanced and a critically needed part of the world communications ecosystem.   </p>
+
+        <p>Mr. Berliner concluded, “The massive number of satellites being launched by Elon Musk’s SpaceX and others is helping to accelerate the growth of the satellite industry. The satellite business is the next great leap forward for high-speed data to reach everyone on earth and I look forward to the day when Digital Locations may play a critical role in the infrastructure of the future”. </p>
+
+
+        <h2>About Digital Locations, Inc.</h2>
+        <p>Digital Locations, Inc., is the developer of Satenna™, a breakthrough technology that will deliver high-speed Internet from satellites directly to smartphones all over the world. Today, high-speed Internet (also known as broadband Internet) is only available in densely populated areas around the world. The rest of the world is still waiting. Elon Musk (SpaceX), Jeff Bezos (Blue Origin), and others are launching thousands of Low Earth Orbit (LEO) satellites. Unfortunately, without the use of additional equipment, the technology does not yet exist that will allow these satellites to deliver high-speed Internet directly to smartphones. Solving this problem represents an extraordinary business opportunity. Working with a research team from Florida International University, we are developing a truly disruptive technology that will finally make it possible for the wireless industry to offer high-speed Internet service from satellites directly to smartphones anywhere in the world, with no dead zones and no cell towers. Anyone, anywhere, regardless of location, economic status, race, or gender will be able to access high-speed Internet service and benefit from remote learning, health care, government services, telework, participation in public affairs, and even Netflix, Amazon, and much, much more.
+
+        </p>
+        <p>To learn more about Digital Locations please visit <a href="https://www.digitallocations.com/" target="_blank" rel="noopener noreferrer"> www.digitallocations.com</a> </p>
+        <h2>Safe Harbor Statement</h2>
+        <p>This press release may contain “forward-looking statements.” Forward-looking statements are neither historical facts nor assurances of future performance. Instead, they are based only on our current beliefs, expectations, and assumptions regarding the future of our business, future plans and strategies, projections, anticipated events and trends, the economy, and other future conditions. Because forward-looking statements relate to the future, they are subject to inherent uncertainties, risks, and changes in circumstances that are difficult to predict and many of which are outside of our control. Our actual results and financial condition may differ materially from those indicated in the forward-looking statements. Therefore, you should not rely on any of these forward-looking statements. Any forward-looking statement made by us in this release is based only on information currently available to us and speaks only as of the date on which it is made. We undertake no obligation to publicly update any forward-looking statement, whether written or oral, that may be made from time to time, whether as a result of new information, future developments, or otherwise, except as may be required under applicable law.
+        </p>
+        <p>
+            Press Contact: <br>
+            <a href="mailto:communications@digitallocations.com">communications@digitallocations.com</a> <br>
+            (805) 456-7000
+
+        </p>
+    </div>
+</div>
+
+
+<?php
+include('footer.php'); ?>
